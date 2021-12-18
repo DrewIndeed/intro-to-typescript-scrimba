bbbc65a36d9372169addf2623cbf40dc97a85391
Andrew Le <s3820098@rmit.edu.vn>
2021-12-18 21:43:10 +0700

    2. Calculator example (before TypeScript)

---
+++ Learn-primitive-types/1-calculator/index.js
@@ -0,0 +1,25 @@
+// Benefits of TypeScript
+// challenges:
+// - give the screen's innerHTML a string
+// - change the two input values to numbers
+
+const button = document.querySelector(".button");
+const firstInput = document.querySelector("#first-input");
+const secondInput = document.querySelector("#second-input");
+const screen = document.querySelector(".screen");
+
+// this will result 56 cause inputs right now are STRINGS
+// function addNumbers(a, b) {
+//   screen.innerHTML = a + b;
+// }
+
+// this will give NUMBER RESULT by using typeof
+function addNumbers(a, b) {
+  if (typeof a !== "number" && typeof b !== "number") {
+    screen.innerHTML = parseInt(a) + parseInt(b);
+  } else screen.innerHTML = a + b;
+}
+
+button.addEventListener("click", () =>
+  addNumbers(firstInput.value, secondInput.value)
+);
