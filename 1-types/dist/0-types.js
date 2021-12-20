a8f5a0d47445e9538da182d39df8bce3c3c1d99a
Andrew Le <s3820098@rmit.edu.vn>
2021-12-20 10:56:29 +0700

    4. More about types

--- 1-types/dist/0-types.js
+++ 1-types/dist/0-types.js
@@ -1,5 +1,48 @@
 "use strict";
+// Basics
 let age = 22; // try to change type to string -> give error
-let firstName = 'Andrew'; // try to change type to number -> give error
+let firstName = "Andrew"; // try to change type to number -> give error
 let isCoupled = true;
-console.log(firstName);
+let x = "Chocolate box";
+// Array with a particular type: type + []
+let array1 = [1, 2, 3, 4, 5];
+// array1.push("Andrew") -> error
+// Array with any types
+let array2 = [1, true, "Andrew"];
+// Tuple
+let person = [22, "Andrew", true];
+// Tuple Array
+let employee = [
+    [1, "Anderson"],
+    [2, "Andrew"],
+    [3, "Andrea"],
+];
+// Union
+let uid;
+uid = 22;
+uid = "Olalala";
+// Enum
+var Direction1;
+(function (Direction1) {
+    Direction1[Direction1["up"] = 0] = "up";
+    Direction1[Direction1["down"] = 1] = "down";
+    Direction1[Direction1["left"] = 2] = "left";
+    Direction1[Direction1["right"] = 3] = "right";
+})(Direction1 || (Direction1 = {}));
+var Direction2;
+(function (Direction2) {
+    Direction2[Direction2["up"] = 1] = "up";
+    Direction2[Direction2["down"] = 2] = "down";
+    Direction2[Direction2["left"] = 3] = "left";
+    Direction2[Direction2["right"] = 4] = "right";
+})(Direction2 || (Direction2 = {}));
+var Direction3;
+(function (Direction3) {
+    Direction3["up"] = "up";
+    Direction3["down"] = "down";
+    Direction3["left"] = "left";
+    Direction3["right"] = "right";
+})(Direction3 || (Direction3 = {}));
+console.log(Direction1.up); // 0
+console.log(Direction2.up); // 1
+console.log(Direction3.up); // up
--- 1-types/src/0-types.ts
+++ 1-types/src/0-types.ts
@@ -1,3 +1,93 @@
-let age: number = 22 // try to change type to string -> give error
-let firstName: string = 'Andrew' // try to change type to number -> give error
-let isCoupled: boolean = true
+// Basics
+let age: number = 22; // try to change type to string -> give error
+let firstName: string = "Andrew"; // try to change type to number -> give error
+let isCoupled: boolean = true;
+let x: any = "Chocolate box";
+
+// Array with a particular type: type + []
+let array1: number[] = [1, 2, 3, 4, 5];
+// array1.push("Andrew") -> error
+
+// Array with any types
+let array2: any = [1, true, "Andrew"];
+
+// Tuple
+let person: [number, string, boolean] = [22, "Andrew", true];
+
+// Tuple Array
+let employee: [number, string][] = [
+  [1, "Anderson"],
+  [2, "Andrew"],
+  [3, "Andrea"],
+];
+
+// Union
+let uid: string | number;
+uid = 22;
+uid = "Olalala";
+
+// Enum
+enum Direction1 {
+  up, // 0
+  down, // 1
+  left, // 2
+  right, // 3
+}
+
+enum Direction2 {
+  up = 1, // 1
+  down, // 2
+  left, // 3
+  right, // 4
+}
+
+enum Direction3 {
+  up = "up", // up
+  down = "down", // down
+  left = "left", // left
+  right = "right", // right
+}
+
+console.log(Direction1.up); // 0
+console.log(Direction2.up); // 1
+console.log(Direction3.up); // up
+
+// Objects with inline type declaration
+const user1: {
+  id: number;
+  name: string;
+} = {
+  id: 22,
+  name: "John",
+};
+
+// Objects with separated type declaration
+type User2 = {
+  id: number;
+  name: string;
+};
+
+const user2: User2 = {
+  id: 22,
+  name: "John",
+};
+
+// Types Assertion (kinda like casting)
+let cid: any = 1;
+// Way 1
+let customerId = <number>cid;
+// Way 2
+let studentId = cid as number;
+
+// Functions
+// 1/ without declaring types for params -> errors
+// to disable that: to config file -> uncomment noImplicitAny: "true"
+// 2/ Must also declare RETURN TYPE
+function add(x: number, y: number): number {
+  return x + y;
+}
+
+// Void functions
+function log(msg: string | number) {
+  console.log(msg);
+}
