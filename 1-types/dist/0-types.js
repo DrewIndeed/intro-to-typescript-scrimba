d4190971875f0c3a756d58404916fcc14e190b29
Andrew Le <s3820098@rmit.edu.vn>
2021-12-20 10:23:39 +0700

    3. Rename files and folders

---
+++ 1-types/dist/0-types.js
@@ -0,0 +1,5 @@
+"use strict";
+let age = 22; // try to change type to string -> give error
+let firstName = 'Andrew'; // try to change type to number -> give error
+let isCoupled = true;
+console.log(firstName);
