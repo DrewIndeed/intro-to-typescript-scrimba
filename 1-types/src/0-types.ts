9467624c6ee64d89d854c3254120774e2d028f33
Andrew Le Nguyen <truongan1730@gmail.com>
2022-03-03 14:55:55 +0700

    19 Minor adjustment: types folder

--- 1-types/src/0-types.ts
+++ 1-types/src/0-types.ts
@@ -1,30 +1,30 @@
 // Basics
 let age: number = 22; // try to change type to string -> give error
-let firstName: string = "Andrew"; // try to change type to number -> give error
+let firstName: string = 'Andrew'; // try to change type to number -> give error
 let isCoupled: boolean = true;
-let x: any = "Chocolate box";
+let x: any = 'Chocolate box';
 
 // Array with a particular type: type + []
 let array1: number[] = [1, 2, 3, 4, 5];
 // array1.push("Andrew") -> error
 
 // Array with any types
-let array2: any = [1, true, "Andrew"];
+let array2: any = [1, true, 'Andrew'];
 
 // Tuple
-let person: [number, string, boolean] = [22, "Andrew", true];
+let person: [number, string, boolean] = [22, 'Andrew', true];
 
 // Tuple Array
 let employee: [number, string][] = [
-  [1, "Anderson"],
-  [2, "Andrew"],
-  [3, "Andrea"],
+  [1, 'Anderson'],
+  [2, 'Andrew'],
+  [3, 'Andrea'],
 ];
 
 // Union
 let uid: string | number;
 uid = 22;
-uid = "Olalala";
+uid = 'Olalala';
 
 // Enum
 enum Direction1 {
@@ -35,17 +35,17 @@ enum Direction1 {
 }
 
 enum Direction2 {
-  up = 1, // 1
+  up = 1, // 1a
   down, // 2
   left, // 3
   right, // 4
 }
 
 enum Direction3 {
-  up = "up", // up
-  down = "down", // down
-  left = "left", // left
-  right = "right", // right
+  up = 'up', // up
+  down = 'down', // down
+  left = 'left', // left
+  right = 'right', // right
 }
 
 console.log(Direction1.up); // 0
@@ -53,13 +53,7 @@ console.log(Direction2.up); // 1
 console.log(Direction3.up); // up
 
 // Objects with inline type declaration
-const user1: {
-  id: number;
-  name: string;
-} = {
-  id: 22,
-  name: "John",
-};
+const user1: { id: number; name: string } = { id: 22, name: 'John' };
 
 // Objects with separated type declaration
 type User2 = {
@@ -69,7 +63,7 @@ type User2 = {
 
 const user2: User2 = {
   id: 22,
-  name: "John",
+  name: 'John',
 };
 
 // Types Assertion (kinda like casting)
@@ -100,15 +94,15 @@ interface UserInterface {
 
 const user3: UserInterface = {
   id: 22,
-  name: "John",
+  name: 'John',
 };
 
 // Interface functions
 interface MathFunc {
   (x: number, y: number): number;
 }
-const addFunc = (x: number, y: number): number => x + y;
-const subFunc = (x: number, y: number): number => x - y;
+const addFunc: MathFunc = (x: number, y: number): number => x + y;
+const subFunc: MathFunc = (x: number, y: number): number => x - y;
 
 // Classes
 class Person {
@@ -127,8 +121,8 @@ class Person {
   }
 }
 // class usage
-let john = new Person(1, "John");
-let bob = new Person(2, "Bob");
+let john = new Person(1, 'John');
+let bob = new Person(2, 'Bob');
 console.log(john, bob);
 console.log(john.register());
 
@@ -163,7 +157,7 @@ class Employee extends Person2 {
   }
 }
 // usage
-const emp1 = new Employee(3, "Anderson", "Developer");
+const emp1 = new Employee(3, 'Anderson', 'Developer');
 console.log(emp1.name);
 console.log(emp1.position);
 console.log(emp1.register());
@@ -176,12 +170,12 @@ function generateArray1(items: any[]): any[] {
 // usage
 let numArr1 = generateArray1([1, 2, 3, 4]);
 let strArr1 = generateArray1([
-  "Andrew",
-  "Anderson",
-  "Sadio Mane",
-  "Mohamed Salah",
+  'Andrew',
+  'Anderson',
+  'Sadio Mane',
+  'Mohamed Salah',
 ]);
-numArr1.push("Nikola"); // no error because of type 'any'
+numArr1.push('Nikola'); // no error because of type 'any'
 console.log(numArr1);
 
 // With generics
@@ -190,10 +184,10 @@ function generateArray2<T>(items: T[]): T[] {
 }
 let numArr2 = generateArray2<number>([1, 2, 3, 4]);
 let strArr2 = generateArray2<string>([
-  "Andrew",
-  "Anderson",
-  "Sadio Mane",
-  "Mohamed Salah",
+  'Andrew',
+  'Anderson',
+  'Sadio Mane',
+  'Mohamed Salah',
 ]);
 // numArr2.push("Nikola"); // will give error because of type 'number'
 numArr2.push(6868); // no error because the input is of number type
